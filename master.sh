034c718f505d610dcbf6dc799dd645a4a5347f97
Denis Andrejew <da.colonel@gmail.com>
2018-08-18 18:31:47 +0200

    update scripts for k8s v1.11.x & current versions of addons; disabling hsts & default https redirects; flannel instead of weave; cert-manager instead of deprecated kube-lego

--- master.sh
+++ master.sh
@@ -4,11 +4,14 @@ set -euxo pipefail
 
 [[ ! -f .env ]] || source .env
 
-[[ -n ${LEGO_CERTS_RESTORE_EMAIL-''} ]] || { echo LEGO_CERTS_RESTORE_EMAIL is not set >&2; exit 1; }
+[[ -n ${ACME_EMAIL-''} ]] || { echo ACME_EMAIL is not set >&2; exit 1; }
 [[ -n ${DOCKER_REGISTRY_HOST-''} ]] || { echo DOCKER_REGISTRY_HOST is not set >&2; exit 1; }
 
+K8S_VERSION="stable-${K8S_VERSION:-$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)}"
+
 # configure k8s master
-kubeadm init
+kubeadm config images pull --kubernetes-version="${K8S_VERSION}"
+kubeadm init --kubernetes-version="${K8S_VERSION}" --pod-network-cidr=10.244.0.0/16
 
 echo Please save the kubeadm join command from the above output to be able to allow other nodes to join in the future.
 read -n 1 -s -r -p "Press any key to continue"
@@ -18,9 +21,8 @@ mkdir -p $HOME/.kube
 cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 chown $(id -u):$(id -g) $HOME/.kube/config
 
-# set up weave network addon
-sysctl net.bridge.bridge-nf-call-iptables=1
-kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
+# flannel CNI plugin
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/v0.10.0/Documentation/kube-flannel.yml
 
 # allow master to also function as worker
 kubectl taint nodes --all node-role.kubernetes.io/master-
@@ -31,7 +33,7 @@ kubectl create ns kube-ext
 # install heapster (metric collector)
 pushd $(mktemp -d /tmp/kube-master.XXX)
 git clone https://github.com/kubernetes/heapster
-pushd heapster/deploy/kube-config
+cd heapster/deploy/kube-config
 sed -r -i '/type: NodePort/ s/#\s*//' influxdb/grafana.yaml
 kubectl apply -n kube-system -f influxdb/
 kubectl apply -n kube-system -f rbac/heapster-rbac.yaml
@@ -39,10 +41,9 @@ kubectl apply -n kube-system -f rbac/heapster-rbac.yaml
 GRAF_PORT=$(kubectl get svc -n kube-system monitoring-grafana -ojsonpath='{.spec.ports[0].nodePort}')
 echo Grafana can be accessed on port $GRAF_PORT, e.g. http://$(hostname -I | awk '{print $1}'):$GRAF_PORT
 popd
-popd
 
 # install helm
-wget https://kubernetes-helm.storage.googleapis.com/helm-v2.8.1-linux-amd64.tar.gz -O helm.tgz
+wget https://kubernetes-helm.storage.googleapis.com/helm-v2.9.1-linux-amd64.tar.gz -O helm.tgz
 tar xfz helm.tgz
 mv linux-amd64/helm /usr/local/bin/
 rm -rf helm.tgz linux-amd64
@@ -52,9 +53,11 @@ kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admi
 # install helm
 # --wait is not reliable, need to wait for rollout to be finished
 helm init --service-account=tiller --wait || true
-kubectl rollout status -w -n kube-system deploy tiller-deploy
+kubectl rollout status -w -n kube-system deploy/tiller-deploy
 # add incubator repo
 helm repo add incubator http://storage.googleapis.com/kubernetes-charts-incubator
+# update repos
+helm repo update
 # ready to install stuff through helm / hub.kubeapps.com now
 
 # install nginx ingress controller
@@ -64,18 +67,24 @@ helm install stable/nginx-ingress --name nginx --namespace kube-ext \
   --set controller.daemonset.useHostPorts=true \
   --set controller.service.type=NodePort \
   --set controller.hostNetwork=true \
-  --set controller.stats.enabled=true
+  --set controller.stats.enabled=true \
+  --set controller.config.hsts=\"false\" \
+  --set controller.config.hsts-include-subdomains=\"true\" \
+  --set controller.config.hsts-max-age=0 \
+  --set controller.config.hsts-preload=\"false\" \
+  --set controller.config.ssl-redirect=\"false\"
 
-# install kube-lego
-helm install --name lego --namespace kube-ext \
+# install cert-manager
+helm install --name cm --namespace kube-ext \
   --set rbac.create=true \
-  --set config.LEGO_EMAIL=${LEGO_CERTS_RESTORE_EMAIL},config.LEGO_URL=https://acme-v01.api.letsencrypt.org/directory \
-    stable/kube-lego
+  --set ingressShim.defaultIssuerName=letsencrypt-prod \
+  --set ingressShim.defaultIssuerKind=ClusterIssuer \
+    stable/cert-manager
+cat master/cert-manager/cluster-issuer.yaml | envsubst | kubectl apply -f -
 
 # install kube dashboard
-# versions >0.5.2 seem bugged in terms of rbac
-helm install stable/kubernetes-dashboard --namespace kube-ext --name kube-dash --version 0.5.2 \
-  --set rbac.create=true
+helm install stable/kubernetes-dashboard --namespace kube-ext --name kube-dash \
+  --set rbac.create=true --set rbac.clusterAdminRole=true
 # in order to access dashboard do the following on your machine:
 # kubectl port-forward -n kube-ext $(kubectl get pods -n kube-ext -l "app=kubernetes-dashboard,release=kube-dash" -o jsonpath="{.items[0].metadata.name}") 9090:9090
 # (or use the provided utils/k8s/dash.sh script)
--- prepare.sh
+++ prepare.sh
@@ -2,6 +2,11 @@
 
 set -euxo pipefail
 
+[[ ! -f .env ]] || source .env
+
+DOCKER_VERSION_PIN=${DOCKER_VERSION_PIN:-"17.03.*"}
+K8S_VERSION=${K8S_VERSION:-$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)}
+
 apt-get update
 
 apt-get install -y \
@@ -25,7 +30,7 @@ apt-get update
 # pin docker release to 17.03.x (highest currently officially supported one)
 cat <<EOF > /etc/apt/preferences.d/docker-ce
 Package: docker-ce
-Pin: version 17.03.*
+Pin: version ${DOCKER_VERSION_PIN}
 Pin-Priority: 1000
 EOF
 
@@ -33,22 +38,26 @@ apt-get install -y docker-ce
 
 # disable swap (kubelet won't start with it)
 swapoff -a
-sed -i '/ swap / s/^/#/' /etc/fstab
+sed -r -i '/^[^#].* swap / s/^/#/' /etc/fstab
+
+# apparently needed for something, docs say only for non-CNI mode with simple networking
+sysctl net.bridge.bridge-nf-call-iptables=1
 
 # pin kubelet
+K8S_MINOR="$(echo $K8S_VERSION | sed -r 's/^v|([0-9]+\.[0-9]+)\..+/\1/g')"
 cat <<EOF > /etc/apt/preferences.d/kube
 Package: kubelet
-Pin: version 1.9.*
+Pin: version ${K8S_MINOR}.*
 Pin-Priority: 1000
 
 Package: kubeadm
-Pin: version 1.9.*
+Pin: version ${K8S_MINOR}.*
 Pin-Priority: 1000
 EOF
 
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
-deb http://apt.kubernetes.io/ kubernetes-xenial main
+deb http://apt.kubernetes.io/ kubernetes-$(lsb_release -c -s) main
 EOF
 apt-get update
 apt-get install -y kubelet kubeadm kubectl

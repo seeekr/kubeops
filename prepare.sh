2bdb5799b29d5cf742bb8d1780d868820c2e2c80
Denis Andrejew <da.colonel@gmail.com>
2018-08-19 17:38:48 +0200

    fix

--- prepare.sh
+++ prepare.sh
@@ -57,7 +57,7 @@ EOF
 
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
-deb http://apt.kubernetes.io/ kubernetes-$(lsb_release -c -s) main
+deb http://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 apt-get update
 apt-get install -y kubelet kubeadm kubectl
